f23e7628435f08066b368cf570a53534f85664c8
peoul <lpeou2@jh.edu>
2025-08-22 15:41:13 -0700

    feat - update routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./pages/Main.tsx";
+
 
 import { RouterProvider } from "react-router/dom";
 import { router } from "./route.tsx";
